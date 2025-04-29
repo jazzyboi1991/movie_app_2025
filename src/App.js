17fd5bba1b20dd44f07033c800d7e23250feb0fe
jazzyboi1991 <funkybot2002@gmail.com>
2025-04-30 02:17:57 +0900

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    console.log('add');
+  };
+
+  sub = () => {
+    console.log('sub');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button>Add</button>
+        <button>Sub</button>
+      </div>
+    )
+  }
 }
 
 export default App;
